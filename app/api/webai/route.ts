fc649ede49ee5f8dd28374749c9d12b2483b46bc
Shubham Sharma <riyashekhar.singh@ssmc.edu.in>
2024-12-30 23:38:37 +0530

    Fixed Webai

--- app/api/webai/route.ts
+++ app/api/webai/route.ts
@@ -1,4 +1,5 @@
 import { VertexAI } from "@google-cloud/vertexai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 import { GoogleGenerativeAIStream, Message, StreamingTextResponse } from "ai";
 const {
   FunctionDeclarationSchemaType,
@@ -6,7 +7,7 @@ const {
   HarmCategory,
 } = require('@google-cloud/vertexai');
 
-const vertex_ai = new VertexAI({ project: 'ai-based-437315', location: 'asia-south1' });
+const vertex_ai = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY || "");
 
 const buildGoogleGenAIPrompt = (messages: Message[]) => ({
   contents: messages
@@ -22,14 +23,14 @@ const buildGoogleGenAIPrompt = (messages: Message[]) => ({
 export async function POST(req: Request) {
   // Extract the `messages` and `model` from the body of the request
   const { messages,  } = await req.json();
-  const model = "gemini-1.5-flash-002"
+  const model = "gemini-1.5-pro-002"
 
   // Dynamically get the model based on the request
-  const generativeModel = vertex_ai.preview.getGenerativeModel({
+  const generativeModel = vertex_ai.getGenerativeModel({
     model: model,
     generationConfig: {
     'maxOutputTokens': 5000,
-    'temperature': 0.1,
+    'temperature': 0.9,
     'topP': 0.95,
   },
   safetySettings: [{category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT, threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE}],
