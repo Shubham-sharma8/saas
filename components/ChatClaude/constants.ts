0308a938a7fd990211d32f746a631e493b06ce75
Shubham Sharma <riyashekhar.singh@ssmc.edu.in>
2025-01-08 18:40:50 +0530

    Claude sonnet lable fix

--- components/ChatClaude/constants.ts
+++ components/ChatClaude/constants.ts
@@ -14,31 +14,31 @@ export const formSchema = z.object({
 export const modelOption = [
   {
     value: "claude-3-5-sonnet-20241022",
-    label: "Claude-3-5-Sonnet-20241022",
+    label: "Claude 3.5 Sonnet Latest",
     data: "Our Most advanced model and it has the same high intelligence as Gemini-1.5 but is much more efficient—it generates text 2x faster. Up to Oct 2024"
   },
   {
     value: "claude-3-5-sonnet-20240620",
-    label: "claude-3-5-sonnet-20240620",
+    label: "claude 3.5 sonnet",
     data: "Our Most advanced model and it has the same high intelligence as Gemini-1.5 but is much more efficient—it generates text 2x faster. Up to Oct 2024"
 
   },
   {
     value: "claude-3-5-haiku-20241022",
-    label: "claude-3-5-haiku-20241022",
+    label: "claude 3.5 haiku",
     data: "Our Most advanced model and it has the same high intelligence as Gemini-1.5 but is much more efficient—it generates text 2x faster. Up to Oct 2024"
 
   },
   {
     value: "claude-3-opus-20240229",
-    label: "claude-3-opus-20240229",
+    label: "claude 3 opus",
   },
   {
     value: "claude-3-sonnet-20240229",
-    label: "claude-3-sonnet-20240229",
+    label: "claude 3 sonnet",
   },
   {
     value: "claude-3-haiku-20240307",
-    label: "claude-3-haiku-20240307",
+    label: "claude 3 haiku",
   },
 ]
