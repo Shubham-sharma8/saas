2acb5ec6bf2f4c7910980f5680ae031ce6f626c4
Shubham Sharma <riyashekhar.singh@ssmc.edu.in>
2025-01-09 16:43:21 +0530

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,8 +24,8 @@ export const viewport: Viewport = {
   ],
   width: 'device-width',
   initialScale: 1,
-  maximumScale: 5,
-  userScalable: true,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 const font = Inter({ subsets: ['latin'] });
